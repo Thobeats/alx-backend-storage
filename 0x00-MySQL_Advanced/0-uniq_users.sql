15c37e233e2a69415dafb91455bac94237884ef7
Iyanu Dayisi <tobiy23@gmail.com>
2024-08-08 22:56:08 +0100

    first commits

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,12 @@
+--Write a SQL script that creates a user table
+---id, integer, never null, auto increment and primary key
+---email, string (255 characters), never null and unique
+---name, string (255 characters)
+--If the table already exists, your script should not fail
+--Your script can be executed on any database
+
+CREATE IF NOT EXISTS TABLE users (
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255)
+)
