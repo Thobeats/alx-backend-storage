0eb388e244ba9c451092304c95887fd2ab2da179
Iyanu Dayisi <tobiy23@gmail.com>
2024-08-08 23:35:52 +0100

    adjust query

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,12 +1,4 @@
 --Write a SQL script that creates a user table
----id, integer, never null, auto increment and primary key
----email, string (255 characters), never null and unique
----name, string (255 characters)
---If the table already exists, your script should not fail
---Your script can be executed on any database
-
-use holberton;
-
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
