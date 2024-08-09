9a31ee813e23b6716b08d906ddf9d74d86e8fb6b
Iyanu Dayisi <tobiy23@gmail.com>
2024-08-09 05:43:51 +0100

    sql functions

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,15 @@
+-- creates a trigger that decreases the quantity of an item after adding a new order
+DROP FUNCTION IF EXISTS SafeDiv;
+DELIMITER $$
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+    IF (b = 0)
+    THEN
+        RETURN (0);
+    ELSE
+        RETURN (a / b);
+    END IF;
+END
+$$ 
+DELIMITER ;
