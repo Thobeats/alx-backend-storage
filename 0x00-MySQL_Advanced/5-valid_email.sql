c9f123ca1b1d0a538a03e41b87db1af9ba3a76f0
Iyanu Dayisi <tobiy23@gmail.com>
2024-08-09 05:26:27 +0100

    valid email

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- creates a trigger that decreases the quantity of an item after adding a new order
+DELIMITER $$ 
+CREATE TRIGGER check_email
+AFTER UPDATE ON users
+FOR EACH ROW
+BEGIN
+IF NEW.email <> OLD.email
+THEN
+    SET NEW.valid_email = 0;
+END IF;
+END
+$$
+DELIMITER ;
