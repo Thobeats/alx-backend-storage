7489c30f2c8f83f7baf085af1f7f238ebe1afd57
Iyanu Dayisi <tobiy23@gmail.com>
2024-08-14 00:33:43 +0100

    log stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Mongo DB with PyMongo"""
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    """ Statistics about Nginx logs stored in MongoDB """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+
+    num_of_logs = nginx_collection.count_documents({})
+    print(f'{num_of_logs} logs')
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print('Methods:')
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f'\tmethod {method}: {count}')
+
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+
+    print(f'{status_check} status check')
