104a4b0a74edadd342ca46a3558f594c9e33cd33
Iyanu Dayisi <tobiy23@gmail.com>
2024-08-14 00:26:38 +0100

    get all schools by a topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,5 +4,5 @@
 
 def schools_by_topic(mongo_collection, topic):
     """ Get all documents that have a specific topic """
-    docs = mongo_collection.find_many({"topics": topic})
+    docs = mongo_collection.find({"topics": topic})
     return list(docs)
