b569de8128d341e41673c7c7a092a33669e4cb51
Iyanu Dayisi <tobiy23@gmail.com>
2024-08-14 00:53:40 +0100

    avg score

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Mongo DB with PyMongo"""
+
+
+def top_students(mongo_collection):
+    """ return all students sorted by average score """
+    top_student = mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ])
+
+    return top_student
