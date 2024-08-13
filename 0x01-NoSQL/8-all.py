9a2fd7aa4220fb95d6f280c908852029eaf720f6
Iyanu Dayisi <tobiy23@gmail.com>
2024-08-13 23:48:03 +0100

    count all with pymongo

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-
+"""Mongo DB with PyMongo"""
 import pymongo
 
 
 def list_all(mongo_collection):
+    """ List all documents in Python """
     doc = mongo_collection.find()
     if doc.count() == 0:
         return []
