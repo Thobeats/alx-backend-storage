280de8ac38d9c4efcde88caff1ffa7b8b59eb70f
Iyanu Dayisi <tobiy23@gmail.com>
2024-08-14 18:38:07 +0100

    updates

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,10 +14,12 @@ class Cache:
     """
 
     def __init__(self) -> None:
+        """initialise the cache class"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, int, float, bytes]) -> str:
+        """store a redis cache"""
         key = str(uuid4())
         self._redis.set(key, data)
         return key
